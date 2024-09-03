001666a217e66a42f14029ddaf6deb8e6cbc82c8
Илья Гирсенок <igirsenok@nota.tech>
2024-09-03 16:10:53 +0300

    правки

---
+++ backend/ecosystem.config.js
@@ -0,0 +1,29 @@
+require('dotenv').config();
+
+const {
+  DEPLOY_USER, DEPLOY_HOST, DEPLOY_PATH, DEPLOY_REF, DEPLOY_REPO, DATABASE_URL, SERVER_PORT, JWT_SECRET_KEY = 'origin/master',
+} = process.env;
+
+module.exports = {
+  apps: [{
+    name: 'mesto-backend',
+    script: './dist/app.js',
+    env_production: {
+      JWT_SECRET_KEY, DATABASE_URL, SERVER_PORT,
+    },
+  }],
+
+  // Настройка деплоя
+  deploy: {
+    production: {
+      user: DEPLOY_USER,
+      host: [DEPLOY_HOST],
+      ref: DEPLOY_REF,
+      ssh_options: 'StrictHostKeyChecking=no',
+      repo: DEPLOY_REPO,
+      path: DEPLOY_PATH,
+      'pre-deploy-local': `scp ./.env ${DEPLOY_USER}@${DEPLOY_HOST}:${DEPLOY_PATH}`,
+      'post-deploy': 'export PATH=$PATH:~/.nvm/versions/node/v18.20.4/bin/ && cd backend/ && npm i && npm run build && pm2 restart ecosystem.config.js',
+    },
+  },
+};
--- backend/src/config.ts
+++ backend/src/config.ts
@@ -1,2 +1,2 @@
-export const { JWT_SECRET = 'JWT_SECRET' } = process.env;
+export const { JWT_SECRET = 'JWT_SECRET_KEY' } = process.env;
 export const { DB_ADDRESS = 'mongodb://localhost:27017/mestodb' } = process.env;
