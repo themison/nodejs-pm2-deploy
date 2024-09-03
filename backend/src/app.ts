470bda8bab17d498fbe50e96150fc739fbd7bc28
Илья Гирсенок <igirsenok@nota.tech>
2024-09-03 23:00:25 +0300

    правки

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -4,7 +4,8 @@ import express from 'express';
 import mongoose from 'mongoose';
 import cookieParser from 'cookie-parser';
 import { errors } from 'celebrate';
-// import cors from 'cors';
+
+import cors from 'cors';
 import errorHandler from './middlewares/error-handler';
 import { DB_ADDRESS } from './config';
 import routes from './routes';
@@ -14,10 +15,15 @@ const app = express();
 mongoose.connect(DB_ADDRESS);
 
 // Только для локальных тестов. Не используйте это в продакшене
-// app.use(cors())
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
+app.use(cors({
+  origin: [
+    'https://mesto.themison.ru',
+    'https://api.mesto.themison.ru',
+  ],
+}));
 app.use(routes);
 app.use(errors());
 app.use(errorHandler);
