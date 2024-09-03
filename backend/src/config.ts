ec3292c789ed11d864bd838ff650184b7c957d8c
Илья Гирсенок <igirsenok@nota.tech>
2024-09-03 17:03:58 +0300

    правки

--- backend/src/config.ts
+++ backend/src/config.ts
@@ -1,2 +1,2 @@
 export const { JWT_SECRET = 'JWT_SECRET_KEY' } = process.env;
-export const { DB_ADDRESS = 'mongodb://0.0.0.027017/mestodb' } = process.env;
+export const { DB_ADDRESS = 'mongodb://0.0.0.0:27017/mestodb' } = process.env;
