92a55fefe2cd633de36053397d41a2805b783179
Илья Гирсенок <igirsenok@nota.tech>
2024-09-03 16:30:59 +0300

    правки

---
+++ frontend/ecosystem.config.js
@@ -0,0 +1,25 @@
+require('dotenv').config();
+
+const {
+  DEPLOY_USER, DEPLOY_HOST, DEPLOY_PATH, DEPLOY_REF, DEPLOY_REPO = 'origin/master',
+} = process.env;
+
+module.exports = {
+  apps: [{
+    name: 'frontend',
+  }],
+
+  // Настройка деплоя
+  deploy: {
+    production: {
+      user: DEPLOY_USER,
+      host: [DEPLOY_HOST],
+      ref: DEPLOY_REF,
+      ssh_options: 'StrictHostKeyChecking=no',
+      repo: DEPLOY_REPO,
+      path: DEPLOY_PATH,
+      'pre-deploy-local': `scp ./.env ${DEPLOY_USER}@${DEPLOY_HOST}:${DEPLOY_PATH}`,
+      'post-deploy': 'export PATH=$PATH:~/.nvm/versions/node/v18.20.4/bin/ && cd frontend/ && npm i && npm run build && pm2 restart ecosystem.config.js',
+    },
+  },
+};
