d56afdd6cdaad04f61a4679494dc16992ab45ee7
Илья Гирсенок <igirsenok@nota.tech>
2024-09-03 22:01:31 +0300

    правки

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -139,6 +139,6 @@ class Api {
   }
   }
   // Замените на адрес вашего бэкенда
-  const api = new Api('http://api.mesto.themison.ru');
+  const api = new Api('https://api.mesto.themison.ru');
 
   export default api;
