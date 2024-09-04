4f66a64a247c10c722e8977465bb84cc5827abf0
Илья Гирсенок <igirsenok@nota.tech>
2024-09-04 10:28:03 +0300

    правки

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -19,7 +19,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(routes);
-app.use(cors());
+// app.use(cors());
 app.use(errors());
 app.use(errorHandler);
 
